eac8961f4ad4e80c8e10a0609456cc19e2cfd617
Arnold Briñez <jgb199703@gmail.com>
2024-01-16 23:24:26 -0500

    agregando alertas

---
+++ js/app.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    //seleccionando los elementos de la intergaz 
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
+    
+    eventos();
+
+    function eventos(){
+        inputEmail.addEventListener('blur', validar);
+        inputAsunto.addEventListener('blur', validar);
+        inputMensaje.addEventListener('blur', validar);
+    }
+
+
+    
+
+    function validar(e){
+        const tipo = e.target.id;
+        const campo = e.target.parentElement;
+        console.log(campo);
+        console.log(tipo);
+        const input = e.target.value;
+        if(input.trim() === ""){
+            mostrarAlerta(tipo, campo);
+        }else{
+            console.log("tiene algo")
+        }
+
+    }
+
+
+    function mostrarAlerta(mensjae, campo){
+       
+        //creadno la alerta 
+        const id = campo.lastElementChild.id;
+        console.log(id);
+        const error = document.createElement('p');
+        error.style.textAlign = 'center'  
+        error.style.backgroundColor = 'red'   
+        error.style.color = 'white'  
+        error.style.borderRadius = '10px'
+        
+        error.textContent = `el campo ${mensjae} no puede estar vacio `; 
+        if(!error.classList.contains('invalid')){
+            campo.appendChild(error);
+            error.classList.add('invalid');
+        }else{
+            campo.removeChild(error);
+        }
+        
+       
+        
+        //inyectar el error al formulario 
+        
+
+    }
+
+    function validarCorreo(correo){
+        const regex =   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if(regex.test(correo)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
