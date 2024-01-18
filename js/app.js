fdcf8e42e483f0faea2949112a590b542bc0fda9
Arnold Briñez <jgb199703@gmail.com>
2024-01-18 09:18:23 -0500

    validar cada campo listo

--- js/app.js
+++ js/app.js
@@ -5,40 +5,81 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
-    
+    const botonEnviar = document.querySelector('#botones');
+    const email ={
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
+    console.log(email);
     eventos();
 
     function eventos(){
-        inputEmail.addEventListener('blur', validar);
-        inputAsunto.addEventListener('blur', validar);
-        inputMensaje.addEventListener('blur', validar);
+        inputEmail.addEventListener('input', validar);
+        inputAsunto.addEventListener('input', validar);
+        inputMensaje.addEventListener('input', validar);
     }
-
-
     
 
     function validar(e){
+       
         const tipo = e.target.id;
         const campo = e.target.parentElement;
         const input = e.target.value;
         if(input.trim() === ""){
             mostrarAlerta(tipo, campo);
-        }else{
-            console.log("tiene algo")
+            email[tipo] ='';
+            comporbarEmail(email);
+            return ;
         }
-
+        console.log(validarCorreo(email));
+        console.log(email);
+        if(!validarCorreo(input) && tipo ==="email" ){
+            mostrarAlerta(`el campo email debe ser valido`, campo);
+            email[tipo] ='';
+            comporbarEmail(email)
+            return; 
+        }
+        limpiarAlerta(campo);
+        //asignar los valores al objeto email
+        email[tipo] = input.trim().toLowerCase();
+        comporbarEmail(email); //
+       
+        
     }
 
-
-    function mostrarAlerta(mensjae, campo){
-       
-        //creadno la alerta 
+    function limpiarAlerta(campo){
+        //si existe una alerta se remueve del campo (referencia)
         const existe = campo.querySelector('.invalid');
         console.log(existe);
         if(existe){
             existe.remove();
+
         }
  
+    }
+
+    function comporbarEmail(email) {
+        //verifica si hay algun campo vacio del objeto email
+       const valores = Object.values(email).includes('');
+       if(!valores){
+            botonEnviar.children[0].classList.remove('opacity-50');
+            botonEnviar.children[0].disabled = false;
+            return;
+        }
+
+        botonEnviar.children[0].classList.add('opacity-50');
+        botonEnviar.children[0].disabled = true;
+
+        
+
+    }
+
+    function mostrarAlerta(mensjae, campo){
+       
+        //creadno la alerta 
+        limpiarAlerta(campo);
         const error = document.createElement('p');
         error.style.textAlign = 'center'  
         error.style.backgroundColor = 'red'   
@@ -50,8 +91,6 @@ document.addEventListener("DOMContentLoaded", () => {
         
         //validando si ya exixte una alerta
         
-       
-        
         //inyectar el error al formulario 
         campo.appendChild(error);
 
@@ -59,11 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function validarCorreo(correo){
         const regex =   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        if(regex.test(correo)){
-            return true;
-        }else{
-            return false;
-        }
+       return regex.test(correo)
     }
 
 
